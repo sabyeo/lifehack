c88e4efea68093699f5f64e50838077a581b1cf2
yeonatalie <yeonatalie@yahoo.com.sg>
2021-07-22 21:23:18 +0800

    Navigation bar

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+   <div id="main">
+    <header>
+        <nav>
+        <h1><router-link to="/" exact id="index">{{ msg }}</router-link></h1>
+        <div class="links">
+          <a>
+            <router-link to="/about" exact>About Us</router-link>
+          </a>
+          <a>
+            <router-link to="/link3" exact>Link 3</router-link>
+          </a>
+          <a>
+            <router-link to="/link2" exact>Link 2</router-link>
+          </a>
+          <a>
+            <router-link to="/link1" exact>Link 1</router-link>
+          </a>
+        </div>
+        </nav>
+    </header>
+   </div>
+  
+</template>
+
+<script>
+import $ from "jquery";
+export default {
+  data(){
+    return{
+        msg: 'LifeHack 2021'
+    }
+  },
+  mounted() {
+    $(document).ready(function() {
+      $(".ui  .item").on("click", function() {
+        console.log("removed");
+        $(".ui .item").removeClass("active");
+        $(this).addClass("active");
+      });
+    });
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#main{
+    background-color: #FFDDD2;
+    border-bottom-style: solid;
+    border-bottom-color: white;
+    border-bottom-width: 1px;
+    height:80px;
+    width: 100%;
+    top: 0;
+    padding-bottom: 15px;
+}
+
+h1 {
+    float: left;
+    margin-left: 2vw;
+    text-align: center;
+    font-weight:bolder;
+    font-size:40px;
+    text-decoration: none;
+    font-family: "Arial Hebrew";
+}
+
+a{
+  color:#E29578;
+  text-decoration: none;
+  float: right;
+  margin-right: 10px;
+}
+
+.links{
+  padding-right: 10px;
+  float: right;
+  margin-top: 55px;
+}
+</style>
