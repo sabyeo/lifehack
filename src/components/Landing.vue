80de735dc3c8a7bdd765faa8042dac8e74ecdfa4
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2021-07-23 17:23:33 +0800

    SignUp-SignIn-Landing

---
+++ src/components/Landing.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+  <div class="main">
+    <h1 id="title">Dashboard</h1> 
+      <p>This page is only visible to users that are currently logged in</p>
+  </div>
+</template>
+
+<script>
+  export default {
+       beforeCreate: function() {
+          document.body.className = 'about';
+      }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p{
+    position: relative;
+    text-align: left;
+    margin-left: 10%;
+    
+}
+#desc{
+    font-size: 30px;
+    
+}
+
+h1{
+    position:relative;
+    margin-left: 10%;
+    margin-top:10%;
+    text-align: left;
+    font-size:60px;
+    font-weight: lighter;
+}
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Test_Firebase from './components/Test_Firebase.vue'
+import Sign_Up from './components/Sign_Up.vue'
+import Sign_In from './components/Sign_In.vue'
+import Landing from './components/Landing.vue'
 
 export default[
     {path:'/',component:Home},
     {path:'/about',component:About},
-    {path:'/test_firebase',component:Test_Firebase}
+    {path:'/test_firebase',component:Test_Firebase},
+    {path:'/sign_up',component:Sign_Up},
+    {path:'/sign_in',component:Sign_In},
+    {path:'/landing',component:Landing}
 ]
