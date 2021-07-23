8083bb7f8ddb2c1ce7c319c16b118dc1ddaa19d3
yeonatalie <yeonatalie@yahoo.com.sg>
2021-07-23 23:29:24 +0800

    Friend page with updating of database

--- src/components/Friend.vue
+++ src/components/Friend.vue
@@ -1,18 +1,82 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <div v-for="friend in otherUsers" v-bind:key="friend.accountDetail.email">
+      <FriendProfileFull v-bind:friend="friend" v-bind:ownModules="userModules"></FriendProfileFull>
+    </div>
   </div>
 </template>
 
 <script>
+import { database } from "@/firebase/";
+import FriendProfileFull from './FriendProfileFull.vue';
+
 export default {
-  name: 'Home',
+  name: 'Friend',
   data(){
     return{
-      msg: 'Friend Page'
+      msg: 'Friend Page',
+      userModules: [],
+      otherUsers: [],
       }
-  } 
-}
+  },
+  components: {
+    FriendProfileFull,
+  },
+  methods: {
+    fetchItems: function () {
+      database
+        .collection("user")
+        .doc('UA2rWxwH2XPxOypfWGZP') //TO REPLACE HARDCODE
+        .get()
+        .then(documentSnapshot => {
+          if (documentSnapshot.exists) {
+            // get modules
+            this.userModules = documentSnapshot.data().accountDetail.modules
+            console.log('User Modules')
+            console.log(this.userModules)
+          }
+        });
+    },
+    getOtherUsers: function() {
+      database
+        .collection("user")
+        .get()
+        .then(querySnapshot => {
+          let item = {};
+          querySnapshot.forEach((doc) => {
+            item = doc.data();
+            if (item.accountDetail.email != 'nat@gmail.com') {//TO REPLACE HARDCODE
+              this.otherUsers.push(item)
+            }
+          });
+        })
+      
+      console.log(this.otherUsers)
+    },
+    // getSimilarUsers: function() {
+    //   let similarUserList = []
+    //   for (const user in otherUsers) {
+    //     console.log('iterating')
+    //     const otherModules = user.accountDetail.modules
+    //     const similarity = this.userModules.filter(value => otherModules.includes(value))
+    //     similarUserList.push({
+    //       email: user.accountDetail.email,
+    //       commonModules: similarity
+    //     })
+    //   }
+    //   similarUserList.sort((a, b) => (a.commonModules.length > b.commonModules.length) ? 1 : -1)
+    //   console.log('Similar User')
+    //   console.log(similarUserList)
+    // },
+  },
+  mounted() {
+      this.fetchItems();
+      this.getOtherUsers()
+  },
+  created() {
+  }
+} 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import Home from './components/Home.vue'
 import Main from './components/Main.vue'
 import About from './components/About.vue'
 import Test_Firebase from './components/Test_Firebase.vue'
+import Friend from './components/Friend.vue'
 
 export default[
     {path:'/',component:Home},
     {path:'/main',component:Main},
+    {path:'/friend',component:Friend},
     {path:'/about',component:About},
     {path:'/test_firebase',component:Test_Firebase}
 ]
